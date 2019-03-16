f04708496c443a2fb7790a0bd21acf97ae79c48c
Bartosz <bartosz.hejke@gmail.com>
2019-03-16 19:49:48 +0100

    Post board update.

--- app/src/main/java/com/bartekhejke/blogapp/HomeFragment.java
+++ app/src/main/java/com/bartekhejke/blogapp/HomeFragment.java
@@ -8,8 +8,10 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
@@ -31,6 +33,7 @@ public class HomeFragment extends Fragment {
     private BlogPostAdapter blogPostAdapter;
 
     private FirebaseFirestore firebaseFirestore;
+    private DocumentSnapshot lastVisible;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -52,7 +55,23 @@ public class HomeFragment extends Fragment {
 
         firebaseFirestore = FirebaseFirestore.getInstance();
 
-        Query firstQuery = firebaseFirestore.collection("Posts").orderBy("timestamp", Query.Direction.DESCENDING);
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                Boolean reachedBottom = !recyclerView.canScrollVertically(-1);
+
+                String desc = lastVisible.getString("desc");
+
+                if (reachedBottom){
+                    Toast.makeText(getContext(), "Reached: "+ desc, Toast.LENGTH_SHORT).show();
+                    morePosts();
+                }
+            }
+        });
+
+        Query firstQuery = firebaseFirestore.collection("Posts").orderBy("timestamp", Query.Direction.DESCENDING).limit(3);
 
         firstQuery.addSnapshotListener(new EventListener<QuerySnapshot>() {
             @Override
@@ -72,4 +91,32 @@ public class HomeFragment extends Fragment {
         return view;
     }
 
+    public void morePosts(){
+
+        Query nextQuery = firebaseFirestore.collection("Posts")
+                .orderBy("timestamp", Query.Direction.DESCENDING)
+                .startAfter(lastVisible)
+                .limit(3);
+
+        nextQuery.addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+
+                if (!queryDocumentSnapshots.isEmpty()) {
+                    lastVisible = queryDocumentSnapshots.getDocuments().get(queryDocumentSnapshots.size() - 1);
+
+                    for (DocumentChange doc : queryDocumentSnapshots.getDocumentChanges()) {
+                        if (doc.getType() == DocumentChange.Type.ADDED) {
+
+                            BlogPost blogPost = doc.getDocument().toObject(BlogPost.class);
+                            blogList.add(blogPost);
+                            blogPostAdapter.notifyDataSetChanged();
+                        }
+                    }
+
+                }
+            }
+        });
+
+    }
 }
